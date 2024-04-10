d058996fc7eb66c96319743182d2ddb58378e0c0
Rafael Freire <rafaeldacostafreire@gmail.com>
2024-04-09 23:22:07 -0300

    test: :construction: wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
+import GlobalStyles from "@/styles/GlobalStyles";
+// import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <ChakraProvider>
+
+      <Component {...pageProps} />;
+      </ChakraProvider>
+    </>
+  )
 }
