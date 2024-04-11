39dd0858bcdc388c03adb92b3072b4a4554cf67b
Rafael Freire <rafaeldacostafreire@gmail.com>
2024-04-10 22:09:20 -0300

    test: :construction: wip

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import * as S from './styles';
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <>
+      <S.Wrapper>
+        <Image
+          src="./logo-header.png"
+          width={45}
+          height={39}
+          alt="Picture of the author"
+        />
+        <S.Navbar>
+          <S.NavbarItem>Início</S.NavbarItem>
+          <S.NavbarItem>ONG&apos;s</S.NavbarItem>
+          <S.NavbarItem>Empresa</S.NavbarItem>
+          <S.NavbarItem>Fazer parte</S.NavbarItem>
+        </S.Navbar>
+      </S.Wrapper>
+    </>
+  );
+}
